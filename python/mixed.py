3a3ebb9d4eca7a5ef00e02abe3afc36cae6c1280
Samvel Shegiyan <shegiyansam@gmail.com>
2023-03-31 15:59:51 +0400

    This is .py file with mixed codes

---
+++ python/mixed.py
@@ -0,0 +1,50 @@
+#There are the present codes
+
+#Find min in list
+    # import random
+    #
+    # n = random.randint(1, 10)
+    # arr = []
+    # for i in range(n):
+    #     i = random.randint(10, 100)
+    #     arr.append(i)
+    # print(f"List: {arr}")
+    # print(f"This is min number of list: {min(arr)}")
+#Find max in list
+    # import random
+    #
+    # n = random.randint(1, 10)
+    # arr = []
+    # for i in range(n):
+    #     i = random.randint(10, 100)
+    #     arr.append(i)
+    # print(f"List: {arr}")
+    # print(f"This is max number of list: {max(arr)}")
+
+#Find sum of element in list
+    # import random
+    #
+    # n = random.randint(1, 10)
+    # arr = []
+    # for i in range(n):
+    #     i = random.randint(10, 100)
+    #     arr.append(i)
+    # print(f"List: {arr}")
+    # print(f"This is sum number of list: {sum(arr)}")
+
+#Find odd numbers in list
+    # import random
+    #
+    # n = random.randint(1, 10)
+    # arr = []
+    # for i in range(n):
+    #     i = random.randint(10, 100)
+    #     arr.append(i)
+    # print(f"List: {arr}")
+    # odd = []
+    # for j in arr:
+    #     if j % 2 == 1:
+    #         odd.append(j)
+    # print(f"This is odd numbers list from 'arr': {odd}")
+
+
