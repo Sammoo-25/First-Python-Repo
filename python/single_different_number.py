c5d8b7e757c6e16e4001c21e4d1862f2152952bf
Samvel Shegiyan <shegiyansam@gmail.com>
2023-04-01 14:04:46 +0400

    The bug of previus code was fixed

--- python/single_different_number.py
+++ python/single_different_number.py
@@ -1,20 +1,7 @@
 def stray(arr):
-    # sort_list = sorted(arr)
-    # j = 0
-    # for i in range(len(sort_list) - 1):
-    #     j += 1
-    #     if sort_list[i] == sort_list[j]:
-    #         continue
-    #     else:
-    #         print(f"Single element of list: {sort_list[j]}")
-    #         break
-    a = arr[0]
-    for i in range(len(arr) + 1):
-        if a == i:
-            continue
-        else:
-            print(f"Single element of list: {arr[i]}")
-            break
+    for i in arr:
+        if arr.count(i) == 1:
+            print(f"Single element of list: {i}")
 
 
-stray([1, 1, 1, 1, 5, 1, 1])
+stray([1, 1, 1, 1, 1, 1, 54])
