4c825501a7274fba184df119b94438403820670e
Samvel Shegiyan <shegiyansam@gmail.com>
2023-04-02 14:57:57 +0400

    code about order sentense with numbers in words (your_order_please.py)

---
+++ python/Your_order_please.py
@@ -0,0 +1,16 @@
+def order(sentence):
+    arr = sentence.split()
+    ls = []
+    for i in arr:
+        for j in i:
+            if j.isnumeric():
+                ls.append(int(j))
+    for i in range(len(ls) - 1):
+        for k in range(len(ls) - i - 1):
+            if ls[k] > ls[k + 1]:
+                ls[k], ls[k + 1] = ls[k + 1], ls[k]
+                arr[k], arr[k + 1] = arr[k + 1], arr[k]
+    return ' '.join(arr)
+
+
+print(order("is2 Thi1s T4est 3a"))
