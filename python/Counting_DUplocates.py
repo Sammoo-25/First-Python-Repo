95036e8c4b4bf4e47277e91df21a49ecebf61609
Samvel Shegiyan <shegiyansam@gmail.com>
2023-04-02 16:19:03 +0400

    Code about counting duplicates in stringgs!

---
+++ python/Counting_DUplocates.py
@@ -0,0 +1,10 @@
+def duplicate_count(text):
+    li = list(text.lower())
+    res = {}
+    count = 0
+    for i in li:
+        res[i] = li.count(i)
+    for key in res:
+        if res[key] > 1:
+            count += 1
+    return count
