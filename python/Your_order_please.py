b499db72bab994b6af5e2122142df54243bd9410
Samvel Shegiyan <shegiyansam@gmail.com>
2023-04-02 15:02:41 +0400

    Changing in Your... file by dev_q branch

--- python/Your_order_please.py
+++ python/Your_order_please.py
@@ -11,6 +11,8 @@ def order(sentence):
                 ls[k], ls[k + 1] = ls[k + 1], ls[k]
                 arr[k], arr[k + 1] = arr[k + 1], arr[k]
     return ' '.join(arr)
-
+print("This is dev_q ")
+print("dev_q")
+print("branch")
 
 print(order("is2 Thi1s T4est 3a"))
