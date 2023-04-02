c1385acb0b3b8d033f870d5746d061992923af81
Samvel Shegiyan <shegiyansam@gmail.com>
2023-04-02 14:19:26 +0400

    Changes in Hum... file

--- python/Human_Readable_Time.py
+++ python/Human_Readable_Time.py
@@ -2,4 +2,4 @@ def make_readable(seconds):
     h= seconds/60**2
     m= (seconds%60**2)/60
     s= (seconds%60**2%60)
-    return "%02d:%02d:%02d" % (h, m, s)
+    return "%02d:%02d:%02d" % (s, m ,h)
